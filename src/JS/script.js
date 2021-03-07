9bf04517b8ded37d4b90d152cc6f90810c5c2cc4
William James <52788612+william-james-pj@users.noreply.github.com>
2021-03-07 19:58:37 -0300

    Update script.js

--- src/JS/script.js
+++ src/JS/script.js
@@ -3,6 +3,8 @@ const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElem = document.querySelectorAll(".has-fade");
+const a_menu = document.querySelectorAll(".header__menu a");
+const menuItems = document.querySelectorAll('header a[href^="#"]');
 
 btnHamburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
@@ -24,4 +26,34 @@ btnHamburger.addEventListener("click", function () {
       element.classList.add("fade-in");
     });
   }
+});
+
+a_menu.forEach(a => {
+  a.addEventListener("click", function () {
+    header.classList.remove("open");
+    body.classList.remove("noscroll");
+    fadeElem.forEach((element) => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  });
+})
+
+function getScrollTopByHref(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+function scrollToPosition(to) {
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  });
+}
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const to = getScrollTopByHref(event.currentTarget);
+  scrollToPosition(to);
+}
+menuItems.forEach((item) => {
+  item.addEventListener("click", scrollToIdOnClick);
 });
